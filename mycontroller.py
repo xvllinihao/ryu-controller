0dcba0262399517793bcedf217bbff17a2db2976
ahaWDY <38500681+ahaWDY@users.noreply.github.com>
2021-01-18 22:11:25 +0100

    Update mycontroller.py

--- mycontroller.py
+++ mycontroller.py
@@ -36,19 +36,15 @@ class myswitch13(app_manager.RyuApp):
         self.port_to_switch = defaultdict(dict)  # record the port which connects a switch
 
     # install flow entry
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+    def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
         datapath.send_msg(mod)
 
     # delete all the flow entries except table miss entry of a switch according to datapath
